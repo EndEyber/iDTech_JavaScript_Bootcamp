471c6b256b23ac73e31a89a224f621d790e795df
EndEyber <105532758+EndEyber@users.noreply.github.com>
2022-05-14 12:40:35 +0800

    Add files via upload

---
+++ Basic Data Types Activity/basic_datatypes.js
@@ -0,0 +1,29 @@
+let greeting = "Hello and welcome to the World of Alchemistic"
+let name = "Aroz"
+let story = "Adventure of Alchemical"
+let worldName = "World of Alchemistic"
+
+let age = 15000;
+let year = 1500;
+let century = "2 BC"
+
+let inventory = ['Wooden Sword', 'Torch', 'Biscuits', 'Flask of Water', 'Bandage Kit']
+
+let upgradeStore = true;
+
+console.log(greeting + ". My name is " + name)
+console.log("Aroz: You are part of the story of the: " + story)
+console.log("This is just a tour of the " + story + " , there will be much more to explore through the " + worldName)
+console.log("You are currently in the year " + year + " , and in the century of " + century)
+console.log("I may be " + age + " years old. But my vision stands out to the rest of the " + worldName)
+console.log("It seems you don't have any weapons, here let me give you this Default Kit")
+console.log("Inside this kit, you will have the following items:")
+console.log(inventory)
+console.log("This will guide you through your journey of " + story)
+console.log("Do you have anymore questions?")
+console.log("Me: Yea, I noticed alot of swords being broken easily; is there a way to fix it in the " + worldName)
+console.log("Aroz: Oh, silly me! I forgot about the most useful tip!")
+console.log("Yes, you can buy, upgrade, sell stuff here!")
+console.log("Me: But where is the upgrade store? Do I just go to any buildings to upgrade it?")
+console.log("Aroz: Its " + upgradeStore + " we have an Upgrade Store, its actualy right behind you")
+console.log("Anyways, thats the end of the tour... Don't worry, its not the last time you will see me; I'll guide you through the " + story)
