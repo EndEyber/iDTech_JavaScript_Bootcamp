1a23b6cd091f0d4c661bc8d894f5d66154bf5796
EndEyber <105532758+EndEyber@users.noreply.github.com>
2022-05-14 12:47:39 +0800

    Add files via upload

---
+++ Fortune Teller Activity/fortuneteller.js
@@ -0,0 +1,10 @@
+let fortune = 100;
+
+if(fortune >= 0 && fortune <= 3)
+console.log("You have chosen a low fortune")
+else if(fortune >= 3 && fortune <= 7)
+console.log("You have chosen an average fortune")
+else if(fortune >= 7 && fortune <= 10)
+console.log("You have chosen a high fortune")
+else(fortune >= 10)
+console.log("Try again")
